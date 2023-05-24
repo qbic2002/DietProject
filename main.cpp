fb7685498d5d7fe6b079f6c171e100eb5570b220
qbic <golovinmax28@gmail.com>
2023-05-24 22:19:32 +0300

    fix: main

--- main.cpp
+++ main.cpp
@@ -28,22 +28,13 @@
 
 int main() {
 
-//    std::string input = "Your ideal weight is {{idealWeight}}\n"
-//                        "{#equalOrLess#}\n"
-//                        "Your current weight {{initWeight}} is less or equal to ideal, but you still can get info about diets_!{!!}sdjfghs{//}\n"
-//                        "\n"
-//                        "$exit$";
-//    diet::TokenContainer tokens;
-//    diet::TemplateTokenizer::tokenize(input, tokens);
-//    std::cout << "\n";
-
-
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
+
     diet::DietApp::getInstance()->init();
+
     diet::DietApp::getInstance()->setViewDir(R"(D:\CPP\DietProject\views)");
 
-//    diet::DietApp::getInstance()->registerController("test", new diet::Test());
     diet::DietApp::getInstance()->registerController("greeting", new diet::DietControllerGreeting());
     diet::DietApp::getInstance()->registerController("initName", new diet::DietControllerInitName());
     diet::DietApp::getInstance()->registerController("setName", new diet::DietControllerSetName());
@@ -65,14 +56,5 @@ int main() {
     diet::DietApp::getInstance()->start();
 
     diet::DietApp::getInstance()->freeMemory();
-
-//    diet::ViewEngine eng;
-//    diet::DietModel model;
-//    model.setField("myName", "Max");
-//    model.setField("last", "Test");
-//    eng.render("D:\\CPP\\DietProject\\test.txt", model);
-//
-//    json j = R"({"mem": 12})"_json;
-//    std::cout << to_string(j["mem"]) << std::endl;
     return 0;
 }
